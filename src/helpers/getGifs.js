1ee913c4975e84aa28860981120af92d1449b621
Karolos10 <89662114+Karolos10@users.noreply.github.com>
2023-02-14 19:40:22 -0500

    Fist commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,15 @@
+export const getGifs = async(category) => {
+
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=SqbjQn5MU54ZS9DODzXHFmSvM9iLh01o&q=${ category }&limit=30`;
+    const resp = await fetch( url );
+    const { data } = await resp.json();
+    
+    const gifs = data.map( img => ({
+        id: img.id,
+        title: img.title,
+        url: img.images.downsized_medium.url
+    }));
+
+    return gifs
+
+}
